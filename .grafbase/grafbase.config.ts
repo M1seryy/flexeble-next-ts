1aafa748e778d5cdaba3475687394413694745e9
viktorhachkajlo <nagibatartar@gmail.com>
2025-07-05 12:53:43 +0300

    new schema

--- .grafbase/grafbase.config.ts
+++ .grafbase/grafbase.config.ts
@@ -1,25 +1,10 @@
-import { g, config, auth } from '@grafbase/sdk'
+import { g, config } from '@grafbase/sdk'
 
-// Визначаємо модель User
-const User = g.model('User', {
-  name: g.string().length({ min: 2, max: 100 }),
-  email: g.string().unique(),
-  avatarUrl: g.url().optional(),
-})
-
-// Налаштовуємо JWT аутентифікацію (секрет береться із змінної середовища)
-const jwt = auth.JWT({
-
-
-
-  issuer: 'grafbase',
-  secret: g.env('JWT_SECREdfeT')
+const Post = g.model('Post', {
+  title: g.string(),
+  content: g.string()
 })
 
 export default config({
-  schema: g,         // Використовуємо схему
-  auth: {
-    providers: [jwt], // Підключаємо JWT провайдер
-    rules: (rules) => rules.private() // Всі операції — приватні, потребують авторизації
-  },
+  schema: g
 })
